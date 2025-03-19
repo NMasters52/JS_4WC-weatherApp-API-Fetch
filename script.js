8595aad8d67b517fd792aff9cf6e5df0dac7d7cd
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-19 15:32:36 +0000

    add initial fetchData function

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const apiKey = '3c50db9475a0c00809feadc5131454e5'; 
 const city = 'Jacksonville,FL,US'; 
 const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+
+const display = document.getElementById('display');
+
+function fetchData() {
+    fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => displayWeather(data))
+    .catch(error => displayError(error))
+};
+
+function displayWeather(data) {
+    display.innerText = `Success: ${data.name}`;
+};
+
+function displayError(error) {
+    display.innerText = `Error: ${error}`
+}
+
+fetchData();
