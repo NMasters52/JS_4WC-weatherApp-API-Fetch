56eb5f0e1e8634eb2de727c50608604c52cf87e9
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-19 15:51:59 +0000

    feat. webpage displaying data from api

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const city = 'Jacksonville';
 const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
 
-const display = document.getElementById('display');
+const display = document.getElementById('weatherContainer');
 
 function fetchData() {
     fetch(apiUrl)
@@ -13,7 +13,12 @@ function fetchData() {
 };
 
 function displayWeather(data) {
-    display.innerText = `Success: ${data.name}`;
+    display.innerHTML = `
+         <h2 class="cityName">${data.name}</h2>
+        <h3 class="weatherDesc">${data.weather[0].description}</h3>
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="${data.weather[0].description}">
+        <h5 class="weatherTemp">${data.main.temp}</h5>
+`;
 };
 
 function displayError(error) {
