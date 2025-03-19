087cf3b70b7172ec9dc2f334d144b772a8be9e56
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-19 16:12:38 +0000

    error handling added. Fix bug with display error function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = '3c50db9475a0c00809feadc5131454e5'; 
+const apiKey = //your api key here. sign up for one at https://home.openweathermap.org/users/sign_up
 const city = 'Jacksonville'; 
 const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
@@ -6,23 +6,27 @@ const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=i
 const display = document.getElementById('weatherContainer');
 
 function fetchData() {
-    fetch(apiUrl)
-    .then(res => res.json())
-    .then(data => displayWeather(data))
-    .catch(error => displayError(error))
+            fetch(apiUrl)
+                .then(res => {if (res.ok) {
+                   return res.json()
+                }else {
+                    display.innerHTML = `<p>Fetch Error: ${res.status}</p>`;
+                }})
+                .then(data => displayWeather(data))
+                .catch(error => displayError(error))
 };
 
 function displayWeather(data) {
     display.innerHTML = `
-         <h2 class="cityName">${data.name}</h2>
+        <h2 class="cityName">${data.name}</h2>
         <h3 class="weatherDesc">${data.weather[0].description}</h3>
         <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="${data.weather[0].description}">
-        <h5 class="weatherTemp">${data.main.temp}</h5>
+        <h5 class="weatherTemp">${Math.round(data.main.temp)}F</h5>
 `;
 };
 
 function displayError(error) {
-    display.innerText = `Error: ${error}`
+    display.innerHTML = `<p>Error: ${error}</p>`
 }
 
 fetchData();
