03fc2122b799c625d7dfaf8e56eaccacbaaed1b4
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-19 15:41:54 +0000

    Weather API is funcitonal

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiKey = '3c50db9475a0c00809feadc5131454e5'; 
-const city = 'Jacksonville,FL,US'; 
+const city = 'Jacksonville'; 
 const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
 
