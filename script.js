21523e2416d01d84409181732a75aec806fe3aec
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-19 15:25:11 +0000

    add. Html set up. API key and API url set up

---
+++ script.js
@@ -0,0 +1,3 @@
+const apiKey = '3c50db9475a0c00809feadc5131454e5'; 
+const city = 'Jacksonville,FL,US'; 
+const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
