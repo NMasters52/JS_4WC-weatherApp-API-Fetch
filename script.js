1d67dd09eef728ab1c8f2a9a9e6bf23d750d0560
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-03-20 15:36:24 +0000

    took off API key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = '3c50db9475a0c00809feadc5131454e5';
+const apiKey = //refrence readMe file
 const city = 'Jacksonville'; 
 const apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
